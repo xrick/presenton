b56e01a3c079476f7ccaf1d314790254dd89ec31
sauravniraula <developmentsaurav@gmail.com>
2025-09-01 14:31:07 +0545

    fix: ollama pull status sql table

--- servers/fastapi/models/sql/ollama_pull_status.py
+++ servers/fastapi/models/sql/ollama_pull_status.py
@@ -4,6 +4,6 @@ from sqlmodel import Field, Column, JSON, SQLModel, DateTime
 
 
 class OllamaPullStatus(SQLModel, table=True):
-    id: uuid.UUID = Field(primary_key=True, default_factory=uuid.uuid4)
+    id: str = Field(primary_key=True)
     last_updated: datetime = Field(sa_column=Column(DateTime, default=datetime.now))
     status: dict = Field(sa_column=Column(JSON))
