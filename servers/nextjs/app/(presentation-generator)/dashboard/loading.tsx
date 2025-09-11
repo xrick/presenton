7452220e5c5182837c7cafde2bfd415b2baec717
shiva raj badu <shivabadu5@gmail.com>
2025-09-11 14:27:27 +0545

    feat: proper loading in dashboard

--- servers/nextjs/app/(presentation-generator)/dashboard/loading.tsx
+++ servers/nextjs/app/(presentation-generator)/dashboard/loading.tsx
@@ -1,15 +1,28 @@
 import { Skeleton } from '@/components/ui/skeleton'
 import React from 'react'
+import Header from './components/Header'
+import Wrapper from '@/components/Wrapper'
 
 const loading = () => {
     return (
-        <div>
-            <div className='grid grid-cols-4 gap-4  justify-center items-center h-screen'>
-                {Array.from({ length: 10 }).map((_, index) => (
-                    <Skeleton key={index} className="h-full   mx-auto" />
-                ))}
-            </div>
+      <div className=''>
+      <Header />
+      <Wrapper className=''>
+        <div className='container mx-auto px-4 py-8'>
+
+       <h2 className="text-2xl font-roboto font-medium my-6">
+              Slide Presentation
+            </h2>
+      <div className=" mx-auto pb-10 grid xl:grid-cols-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  ">
+        {
+          Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className="h-72 w-full bg-gray-300 aspect-video mx-auto" />
+          ))
+        }
         </div>
+      </div>
+      </Wrapper>
+    </div>
     )
 }
 
